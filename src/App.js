bf45a72f03dcedcc95683e4e3de71ac8198f4058
Alper Turkmen <alper.turkmen@telesmart.io>
2025-01-03 00:58:31 +0300

    Frontend Tailwind ve Routelar eklendi

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useCallback, useEffect, useMemo, useReducer, useState } from "react";
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CourseDetailPage from './pages/CourseDetailPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/course/:id" element={<CourseDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
